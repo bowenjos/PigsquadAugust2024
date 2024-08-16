bc006780b5ce5cb6edc28adca79af25c046a35d4
Ethan Bowen <EthanKBowen@gmail.com>
2024-08-15 22:30:33 -0600

    Arm rotation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,25 +5,41 @@ var power: int = 0
 
 @onready var arrows = [$InputArrow, $InputArrow2, $InputArrow3, $InputArrow4]
 
+var throwing: bool = false
+
+var armRef: Sprite2D
+var rotationSwitch: int = -1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	index = 0
+	armRef = $ShotBotArm
+	#armRef.process_mode = Node.PROCESS_MODE_DISABLED
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(Input.is_action_just_pressed("UpInput")):
-		CheckCurrentArrow(0)
-	elif(Input.is_action_just_pressed("DownInput")):
-		CheckCurrentArrow(1)
-	elif(Input.is_action_just_pressed("LeftInput")):
-		CheckCurrentArrow(2)
-	elif(Input.is_action_just_pressed("RightInput")):
-		CheckCurrentArrow(3)
-	elif(Input.is_action_just_pressed("Throw")):
+	if(!throwing):
+		if(Input.is_action_just_pressed("UpInput")):
+			CheckCurrentArrow(0)
+		elif(Input.is_action_just_pressed("DownInput")):
+			CheckCurrentArrow(1)
+		elif(Input.is_action_just_pressed("LeftInput")):
+			CheckCurrentArrow(2)
+		elif(Input.is_action_just_pressed("RightInput")):
+			CheckCurrentArrow(3)
+		elif(Input.is_action_just_pressed("Throw")):
+			throwing = true
 		pass
-	
+	else:
+		if(Input.is_action_just_released("Throw")):
+			throwing = false
+		armRef.rotation += rotationSwitch*delta
+		if(rotationSwitch < 0 && armRef.rotation <= -PI/2):
+			rotationSwitch = 1
+		elif(rotationSwitch > 0 && armRef.rotation >= 0):
+			rotationSwitch = -1
 
 func ShuffleArrows():
 	for arrow in arrows:
