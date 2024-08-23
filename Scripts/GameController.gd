78da118d046d227d5b64da2d218c76e3b0ad6f95
Ethan Bowen <EthanKBowen@gmail.com>
2024-08-22 20:10:48 -0600

    Shotput distance travled

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -3,6 +3,9 @@ extends Node
 @onready var Counter = $CanvasLayer/Counter
 @onready var remainingTimer = $RemainingTimer
 
+var shotPutSpeed: float = 0
+var shotPutDistanceTraveled: float = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#$ShotPut.launch(Vector2(2400, -236000))
@@ -14,59 +17,61 @@ func _process(delta):
 	if(Input.is_action_just_pressed("Reset")):
 		get_tree().change_scene_to_file("res://Scenes/GameScene.tscn")
 	
-	$Player.speed = $ShotPut.m_groundSpeed
-	$Ground1.speed = $ShotPut.m_groundSpeed
-	$Ground2.speed = $ShotPut.m_groundSpeed
-	$Ground3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky1_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky1_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky1_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky2_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky2_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky2_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky3_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky3_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky3_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky4_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky4_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky4_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky5_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky5_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky5_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky6_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky6_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky6_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky7_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky7_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky7_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky8_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky8_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky8_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky9_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky9_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky9_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky10_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky10_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky10_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky11_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky11_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky11_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky12_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky12_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky12_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky13_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky13_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky13_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_1.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_2.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_3.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_4.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_5.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_6.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_7.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_8.speed = $ShotPut.m_groundSpeed
-	$BackgroundHolder/Sky14_9.speed = $ShotPut.m_groundSpeed
-	$StartingZone.speed = $ShotPut.m_groundSpeed
+	shotPutSpeed = $ShotPut.m_groundSpeed
+	shotPutDistanceTraveled += shotPutSpeed*delta
+	$Player.speed = shotPutSpeed
+	$Ground1.speed = shotPutSpeed
+	$Ground2.speed = shotPutSpeed
+	$Ground3.speed = shotPutSpeed
+	$BackgroundHolder/Sky1_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky1_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky1_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky2_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky2_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky2_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky3_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky3_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky3_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky4_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky4_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky4_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky5_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky5_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky5_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky6_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky6_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky6_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky7_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky7_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky7_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky8_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky8_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky8_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky9_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky9_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky9_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky10_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky10_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky10_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky11_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky11_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky11_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky12_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky12_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky12_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky13_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky13_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky13_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_1.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_2.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_3.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_4.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_5.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_6.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_7.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_8.speed = shotPutSpeed
+	$BackgroundHolder/Sky14_9.speed = shotPutSpeed
+	$StartingZone.speed = shotPutSpeed
 	pass
 
 func _on_shot_put_stop_motion():
